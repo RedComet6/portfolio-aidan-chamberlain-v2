929f529708e81ceae8f67f1162a216ae2ecadd20
Aidan Chamberlain <asunshine99@gmail.com>
2022-05-02 23:30:41 -0500

    updated components and filled out navbar elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,28 @@
 import React from "react";
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <h2 class="navbar-brand">Aidan Chamberlain</h2>
+                {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button> */}
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            About Me
+                        </a>
+                        <a class="nav-link" href="#">
+                            My Works
+                        </a>
+                        <a class="nav-link" href="#">
+                            My Links
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
