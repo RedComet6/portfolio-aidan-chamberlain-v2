1b1940b0fc51b3e29dabdf9c511babd5270880a1
Aidan Chamberlain <asunshine99@gmail.com>
2022-05-02 22:34:53 -0500

    added index.js and App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import "./App.css";
+import React from "react";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Portfolio />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
