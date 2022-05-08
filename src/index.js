7f2bba8bf22e85dcaefceaf315659a084895a6a7
Aidan Chamberlain <asunshine99@gmail.com>
2022-05-08 15:21:58 -0500

    updated index.js to run more current method of react root rendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-// render react content in the body of index.html
-ReactDOM.render(
+// render react content in the body of index.html with the id "root"
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+    </React.StrictMode>
 );
